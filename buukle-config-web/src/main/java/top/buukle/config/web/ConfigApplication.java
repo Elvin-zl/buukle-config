fe8814490f5311a0ecdc2b6e4105b49b7767727c
zhanglei1102 <zhanglei1102@jd.com>
2019-09-23 18:00:34 +0800

    配置中心代码初步完善

---
+++ buukle-config-web/src/main/java/top/buukle/config/web/ConfigApplication.java
@@ -0,0 +1,50 @@
+package top.buukle.config.web;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+@MapperScan({"top.buukle.config.dao","top.buukle.util.mvc"})
+@SpringBootApplication(scanBasePackages={"top.buukle.config.*"})
+@EnableFeignClients(basePackages = {"top.buukle.*"})
+@EnableRedisHttpSession
+public class ConfigApplication {
+    private static volatile boolean RUNNING = true;
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(ConfigApplication.class, args);
+        System.out.println("启动成功");
+        synchronized (ConfigApplication.class) {
+            while (RUNNING) {
+                try {
+                    ConfigApplication.class.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    SpringApplication.exit(context);
+                }
+            }
+        }
+    }
+
+    /**
+     * @description 配置共享session 的domain,cookie
+     * @param
+     * @return org.springframework.session.web.http.CookieSerializer
+     * @Author elvin
+     * @Date 2019/7/30
+     */
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer defaultCookieSerializer = new DefaultCookieSerializer();
+        defaultCookieSerializer.setDomainName("buukle.top");
+        defaultCookieSerializer.setCookieName("BUUKLEUID");
+        defaultCookieSerializer.setCookiePath("/");
+        return defaultCookieSerializer;
+    }
+}
